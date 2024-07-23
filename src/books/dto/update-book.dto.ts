f2b805408f964b53977a97cc107b33106112fd42
christ972 <hisciences331@gmail.com>
2024-07-23 16:09:15 +0200

    crud version 1.0 api

---
+++ src/books/dto/update-book.dto.ts
@@ -0,0 +1,11 @@
+import { PartialType } from '@nestjs/mapped-types';
+import { CreateBookDto } from './create-book.dto';
+
+export class UpdateBookDto extends PartialType(CreateBookDto) {
+    
+    title?:string;
+    author?:string;
+    description?:string;
+    price?:number;
+}
+
